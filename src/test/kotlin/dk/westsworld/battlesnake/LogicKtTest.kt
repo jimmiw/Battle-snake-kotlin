71e5039aaeffa8702e88505cc4d346367399a880
Jimmi Westerberg <jimmi.westerberg@gmail.com>
2022-12-08 23:29:29 +0100

    Fix the logictest... argh :)

--- src/test/kotlin/dk/westsworld/battlesnake/LogicKtTest.kt
+++ src/test/kotlin/dk/westsworld/battlesnake/LogicKtTest.kt
@@ -162,6 +162,6 @@ internal class LogicKtTest {
         // testing get distance between two points, but rounding the result to int, to make it easier to test :)
         assertEquals(3, getDistance(Position(1,1), Position(3,3)).roundToInt())
 
-        assertEquals(3.0, getDistance(Position(1,1), Position(1,2)))
+        assertEquals(1.0, getDistance(Position(1,1), Position(1,2)))
     }
 }
